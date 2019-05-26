3a2a1e11718ce4522e4fcccfde9ffc2934071243
NsajuliAlex <ariaaltex73@gmail.com>
2019-05-26 14:04:28 +0530

    First Commit

---
+++ src/app/booking.ts
@@ -0,0 +1,11 @@
+export class Booking {
+    constructor(
+        public brandName: string,
+        public modelName: string,
+        public vehicleNumber: string,
+        public serviceType: string,
+        public issueNam: string,
+        public describ: string
+
+    ) {}
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+public kanikaContent: any;
+public aboutus: any;
+
+public carBrands = ['ACURA', 'ASTON MARTIN', 'AUDI', 'BENTLEY', 'BMW', 'BUICK',
+'CADILLAC', 'CHEVROLET', 'CHRYSLER', 'DODGE', 'FERRARI', 'FORD', 'GMC', 'HONDA', 'OTHER'];
+public carIssues = ['Engine', 'Tires', 'WindScreen', 'BreakDown'];
+
+  constructor(private buildForm: FormBuilder) {
+
+   }
+
+bookingForm = this.buildForm.group({
+carBrand: ['', Validators.required],
+carModel: [''],
+vehicleNumber: [''],
+serviceType: ['DoorStep'],
+carService: [''],
+describeIssues: ['', Validators.minLength(10) ]
+
+
+});
+  ngOnInit() {
+    this.kanikaContent = {
+      title: 'Remote & DoorStep repair services.',
+      shortdescription: `Kanika Africa's most trusted mechanic service provider`,
+      topNav: [
+        {
+          contact: 'ariaaltex73@gmai.com',
+          icon: 'fa-envelope'
+        },
+        {
+         contact: ' +918335033993'
+        }
+      ],
+      socialIcons: [
+        {
+          icon: 'fa-facebook-square'
+         },
+         {
+          icon: ' fa-instagram'
+         },
+         {
+          icon: 'fa-twitter'
+         },
+         {
+          icon: ' fa-google-plus-square'
+         }
+      ],
+      navigationItems: ['Home', 'Book Service', 'How We Work', 'AboutUs', 'Brands']
+    };
+
+    this.aboutus = {
+      title: 'About Us',
+      description : `Kanika is an African based organisation of certified mechanics that offer both door to door and
+  pick up services to customers from remote locations at the
+  customer's desired time and convinience. Just get in contact with our ecpert mechanics. Someone
+  will
+  be at our location in no time
+  We offer professionalism at a pocket friendly price. Here at Kanika we are here to serve you.
+`
+
+    };
+  }
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({behavior: 'smooth'});
+  }
+
+}
+
+
